3f45100e88c1762491a83e388229ab7d70ac8627
Tony Johnson <tony_johnson@slac.stanford.edu>
2015-07-31 13:21:13 -0700

    Add LSST license to significant files.

--- src/main/java/org/lsst/dm/portal/DMTask.java
+++ src/main/java/org/lsst/dm/portal/DMTask.java
@@ -12,6 +12,27 @@ import org.srs.dataportal.model.Task;
 import org.srs.dataportal.model.User;
 import org.srs.dataportal.model.util.PipelineJob;
 import org.srs.pipeline.client.PipelineClient;
+/*
+LSST Data Management System
+Copyright 2015 AURA/LSST.
+
+This product includes software developed by the
+LSST Project (http://www.lsst.org/).
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the LSST License Statement and
+the GNU General Public License along with this program.  If not,
+see <http://www.lsstcorp.org/LegalNotices/>.
+*/
 
 /**
  * A task for use with Data Management registration portal.
