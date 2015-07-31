01ff5fcc6586c1320d19617460229c1bf475fcc1
Tony Johnson <tony_johnson@slac.stanford.edu>
2015-07-30 17:38:29 -0700

    initial code for filling Jacek's tables.

--- src/main/java/org/lsst/dm/portal/DMTask.java
+++ src/main/java/org/lsst/dm/portal/DMTask.java
@@ -12,9 +12,8 @@ import org.srs.dataportal.model.util.PipelineJob;
 import org.srs.pipeline.client.PipelineClient;
 
 /**
- * A task for use with SimpleSkimmer.
+ * A task for use with Data Management registration portal.
  *
- * @version $Id: SimpleSkimmerTask.java,v 1.24 2012/01/23 22:37:30 bvan Exp $
  */
 public class DMTask extends Task {
 
@@ -43,49 +42,119 @@ public class DMTask extends Task {
         // In this case we do not submit a pipeline job, we just create an 
         // entry in the database table for pending registrations.
         try (Connection connection = config.getConnection()) {
-            PreparedStatement stmt = connection.prepareStatement(
-                    "select id,submitter_name,submitter_email,location,short_name,description,repo_type,owner_type,"
-                            + "ogname,data_release,priority,availability,accessibility,corresponding_type,corresponding_id "
-                            + "from registration_request where id is null",ResultSet.FETCH_FORWARD, ResultSet.CONCUR_UPDATABLE);
-            ResultSet rs = stmt.executeQuery();
-            rs.moveToInsertRow();
-            rs.updateString(2, request.getUserName());
-            rs.updateString(3, request.getEmail());
-            rs.updateString(4, request.getLocation());
-            rs.updateString(5, request.getShortName());
-            rs.updateString(6, request.getDescription());
-            rs.updateString(7, request.getType());
-            rs.updateString(8, request.getOwner());
-            rs.updateString(9, request.getOgname());
-            rs.updateString(10, request.getDataRelease());
-            rs.updateString(11, request.getPriority());
-            rs.updateString(12,request.getAvailability());
-            rs.updateString(13, request.getAccessibility());
-            rs.updateString(14, request.getCorrespondingType());
-            //rs.updateString(15, request.getCorrespondingId());
-            rs.insertRow();
-            // See http://dev.mysql.com/doc/connector-j/en/connector-j-usagenotes-last-insert-id.html
-            rs.last();
-            int id = rs.getInt(1);
-            rs.close();
-            stmt.close();
-            Map<String,String> meta = verifier.getMeta();
-            if (!meta.isEmpty()) {
-                // Now deal with meta-data
-                stmt = connection.prepareStatement("select id,name,value from registration_metadata where id is null",
-                        ResultSet.FETCH_FORWARD, ResultSet.CONCUR_UPDATABLE);
-                rs = stmt.executeQuery();
-                for (Map.Entry<String,String> entry : meta.entrySet()) {
-                    rs.moveToInsertRow();
-                    rs.updateInt(1,id);
-                    rs.updateString(2, entry.getKey());
-                    rs.updateString(3, entry.getValue());
-                    rs.insertRow();
-                }
+            boolean autoCommit = connection.getAutoCommit();
+            try {
+                connection.setAutoCommit(false);
+                int id = recordRegistrationRequest(connection);
+                recordOfficialRequest(connection);
+                connection.commit();
+
+                return new PipelineJob(null, id, null, "LSST-DM", jobName);
+            } finally {
+                connection.setAutoCommit(autoCommit);
             }
-            return new PipelineJob(null, id, null, "LSST-DM", jobName);
         } catch (SQLException ex) {
-            throw new RuntimeException("Error commiting registration request",ex);
+            throw new RuntimeException("Error commiting registration request", ex);
         }
+    }
+
+    /**
+     * Records the registration request (in Tony's temporary tables)
+     *
+     * @param connection The database
+     * @return The id assigned to the record.
+     * @throws SQLException
+     */
+    private int recordRegistrationRequest(final Connection connection) throws SQLException {
+        PreparedStatement stmt = connection.prepareStatement(
+                "select id,submitter_name,submitter_email,location,short_name,description,repo_type,owner_type,"
+                + "ogname,data_release,priority,availability,accessibility,corresponding_type,corresponding_id "
+                + "from registration_request where id is null", ResultSet.FETCH_FORWARD, ResultSet.CONCUR_UPDATABLE);
+        ResultSet rs = stmt.executeQuery();
+        rs.moveToInsertRow();
+        rs.updateString(2, request.getUserName());
+        rs.updateString(3, request.getEmail());
+        rs.updateString(4, request.getLocation());
+        rs.updateString(5, request.getShortName());
+        rs.updateString(6, request.getDescription());
+        rs.updateString(7, request.getType());
+        rs.updateString(8, request.getOwner());
+        rs.updateString(9, request.getOgname());
+        rs.updateString(10, request.getDataRelease());
+        rs.updateString(11, request.getPriority());
+        rs.updateString(12, request.getAvailability());
+        rs.updateString(13, request.getAccessibility());
+        rs.updateString(14, request.getCorrespondingType());
+        //rs.updateString(15, request.getCorrespondingId());
+        rs.insertRow();
+        // See http://dev.mysql.com/doc/connector-j/en/connector-j-usagenotes-last-insert-id.html
+        rs.last();
+        int id = rs.getInt(1);
+        rs.close();
+        stmt.close();
+        Map<String, String> meta = verifier.getMeta();
+        if (!meta.isEmpty()) {
+            // Now deal with meta-data
+            stmt = connection.prepareStatement("select id,name,value from registration_metadata where id is null",
+                    ResultSet.FETCH_FORWARD, ResultSet.CONCUR_UPDATABLE);
+            rs = stmt.executeQuery();
+            for (Map.Entry<String, String> entry : meta.entrySet()) {
+                rs.moveToInsertRow();
+                rs.updateInt(1, id);
+                rs.updateString(2, entry.getKey());
+                rs.updateString(3, entry.getValue());
+                rs.insertRow();
+            }
+        }
+        return id;
+    }
+
+    /**
+     * Records the registration request (in Jacek's official tables)
+     *
+     * @param connection The database
+     * @return The id assigned to the record.
+     * @throws SQLException
+     */
+    private int recordOfficialRequest(Connection connection) throws SQLException {
+        PreparedStatement stmt = connection.prepareStatement(
+                "select repoId,url,shortName,description,repoType,"
+                + "dataRelease ,priorityLevel ,availability,accessibility "
+                + "from Repo where repoId is null", ResultSet.FETCH_FORWARD, ResultSet.CONCUR_UPDATABLE);
+        ResultSet rs = stmt.executeQuery();
+        rs.moveToInsertRow();
+        rs.updateString(2, request.getLocation());
+        rs.updateString(3, request.getShortName());
+        rs.updateString(4, request.getDescription());
+        rs.updateString(5, request.getType());
+        //rs.updateString(8, request.getOwner());
+        //rs.updateString(6, request.getOgname());
+        rs.updateString(6, request.getDataRelease());
+        rs.updateString(7, request.getPriority());
+        rs.updateString(8, request.getAvailability());
+        rs.updateString(9, request.getAccessibility());
+        //rs.updateString(11, request.getCorrespondingType());
+        //rs.updateString(15, request.getCorrespondingId());
+        rs.insertRow();
+        // See http://dev.mysql.com/doc/connector-j/en/connector-j-usagenotes-last-insert-id.html
+        rs.last();
+        int id = rs.getInt(1);
+        rs.close();
+        stmt.close();
+        Map<String, String> meta = verifier.getMeta();
+        if (!meta.isEmpty()) {
+            // Now deal with meta-data
+            stmt = connection.prepareStatement("select repoId,theKey,theValue from RepoAnnotations where repoId is null",
+                    ResultSet.FETCH_FORWARD, ResultSet.CONCUR_UPDATABLE);
+            rs = stmt.executeQuery();
+            for (Map.Entry<String, String> entry : meta.entrySet()) {
+                rs.moveToInsertRow();
+                rs.updateInt(1, id);
+                rs.updateString(2, entry.getKey());
+                rs.updateString(3, entry.getValue());
+                rs.insertRow();
+            }
+        }
+        return id;
     }
 }
